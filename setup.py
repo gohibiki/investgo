17f0a4185961b0aa33d347b1ed88c5247b7e0415
gohibiki <102298969+gohibiki@users.noreply.github.com>
2024-08-28 17:23:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='investgo',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     install_requires=[
         'cloudscraper',
