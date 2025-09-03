240c03bda8658501a7802824a776709e585a8bc6
gohibiki <102298969+gohibiki@users.noreply.github.com>
2025-09-03 12:09:40 +0800

    Update __init__.py

--- investgo/__init__.py
+++ investgo/__init__.py
@@ -10,7 +10,7 @@ from .historical import get_historical_prices, get_multiple_historical_prices
 from .holdings import get_holdings
 from .technical import get_technical_data
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __author__ = "gohibiki"
 __email__ = "gohibiki@protonmail.com"
 __description__ = "A Python library for fetching financial data from Investing.com"
@@ -22,3 +22,4 @@ __all__ = [
     "get_holdings",
     "get_technical_data"
 ]
+
